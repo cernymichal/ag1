428e497e217104720fee54ec855480497269f843
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2022-12-03 02:35:41 +0100

    hw03: solution with recursion

--- hw03/guard.cpp
+++ hw03/guard.cpp
@@ -29,6 +29,61 @@ struct TreeProblem {
 
 #endif
 
-uint64_t solve(const TreeProblem&) {
-    // TODO implement
+#define NULL_MEMO (uint64_t)(-1)
+
+class Solver {
+public:
+    Solver(const TreeProblem& problem) : m_problem(problem), m_connections(problem.gifts.size()), m_memo(memoIndex(problem.gifts.size(), 0), NULL_MEMO) {
+        for (const auto& connection : m_problem.connections) {
+            m_connections[connection.first].push_back(connection.second);
+            m_connections[connection.second].push_back(connection.first);
+        }
+    }
+
+    uint64_t maxGuardedGifts() {
+        if (m_problem.gifts.empty())
+            return 0;
+
+        return maxGuardedGifts(0, 0, 0);
+    }
+
+private:
+    const TreeProblem& m_problem;
+    std::vector<std::vector<ChristmasTree>> m_connections;
+    std::vector<uint64_t> m_memo;
+
+    inline size_t memoIndex(const ChristmasTree& tree, const int currentGroupSize) const {
+        return tree * (m_problem.max_group_size + 1) + currentGroupSize;
+    }
+
+    uint64_t maxGuardedGifts(const ChristmasTree& tree, const ChristmasTree& cameFrom, const int currentGroupSize) {
+        if (m_memo[memoIndex(tree, currentGroupSize)] != NULL_MEMO)
+            return m_memo[memoIndex(tree, currentGroupSize)];
+
+        uint64_t sumWithGuard = 0;
+        uint64_t sumWithoutGuard = 0;
+
+        // with guard
+        if (currentGroupSize < m_problem.max_group_size) {
+            sumWithGuard = m_problem.gifts[tree];
+            for (const auto& connection : m_connections[tree]) {
+                if (connection != cameFrom)
+                    sumWithGuard += maxGuardedGifts(connection, tree, currentGroupSize + 1);
+            }
+        }
+
+        // without guard
+        for (const auto& connection : m_connections[tree]) {
+            if (connection != cameFrom)
+                sumWithoutGuard += maxGuardedGifts(connection, tree, 0);
+        }
+
+        auto max = std::max(sumWithGuard, sumWithoutGuard);
+        m_memo[memoIndex(tree, currentGroupSize)] = max;
+        return max;
+    }
+};
+
+uint64_t solve(const TreeProblem& problem) {
+    return Solver(problem).maxGuardedGifts();
 }

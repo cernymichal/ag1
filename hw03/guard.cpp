58df370ad0e0794a2e12b6cc6b1e41b1ad3d8586
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2022-12-03 01:26:54 +0100

    hw03: add description and sample

---
+++ hw03/guard.cpp
@@ -0,0 +1,34 @@
+#ifndef __PROGTEST__
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cstdint>
+#include <deque>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <optional>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using ChristmasTree = size_t;
+
+struct TreeProblem {
+    int max_group_size;
+    std::vector<uint64_t> gifts;
+    std::vector<std::pair<ChristmasTree, ChristmasTree>> connections;
+};
+
+#endif
+
+uint64_t solve(const TreeProblem&) {
+    // TODO implement
+}
